d8b09853ef84a44bfd8fcc8873798f643e01aa15
Mohd Faheem Khan <faheemkhan@Faheems-MacBook-Air.local>
2026-05-17 15:17:47 +0530

    Updated CORS of server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const app = express();
 // CORS
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL || `shop-ease-nine-sigma.vercel.app`,
+    process.env.CLIENT_URL || 'https://shop-ease-nine-sigma.vercel.app',
+    'https://shop-ease-ochre.vercel.app',
     'http://localhost:5173',
   ],
   credentials: true,
