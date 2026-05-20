d328c726c02994133d87ac1933954b7ff8d11f6d
Mohd Faheem Khan <faheemkhan@Faheems-MacBook-Air.local>
2026-05-20 17:20:41 +0530

    Updated authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -156,10 +156,12 @@ const crypto = require('crypto');
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com', 
+  port: 587,               
+  secure: false,           
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, 
+    pass: process.env.EMAIL_PASS,
   },
 });
 transporter.verify(function (error, success) {
